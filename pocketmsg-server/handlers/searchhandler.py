e1a7d06d7fff1e3a10a0aa293ed449b2e61a6014
Yuriy <monguste@gmail.com>
2018-10-30 23:32:15 +0300

    update search

--- pocketmsg-server/handlers/searchhandler.py
+++ pocketmsg-server/handlers/searchhandler.py
@@ -1,43 +1,29 @@
 from handlers.json_util import JsonHandler
-from database_tools.alchemy import CUsers, CContacts
+from database_tools.alchemy import CUsers
 
 class SearchHandlers(JsonHandler):
     def prepare(self):
         super().prepare()
         self.check_result = self._token_check()
 
-    # def get(self):
-    #     if self.check_result:
-    #         try:
-    #             contact = self.json_data['search']
-    #             search_contact = self.db.query(CUsers).filter(CUsers.email == contact).all()
-    #             if search_contact is None:
-    #                 self.set_status(404, 'User not found')
-    #             else:
-    #                 self.set_status(201, 'Found')
-    #                 self.response['found_username'] = search_contact.username
-    #                 self.write_json()
-    #         except:
-    #             self.send_error(400, reason='No or bad request body')
-    def get(self):
+    def post(self):
         if self.check_result:
             try:
-                contact = self.json_data['search']
-                search_email = self.db.query(CUsers).filter(CUsers.email == contact).all()
-                search_username = self.db.query(CUsers).filter(CUsers.username == contact).all()
-                if search_email:
-                    search_contact = search_email
+                search = self.json_data['search']
+                if search.count('@'):
+                    search_email = self.db.query(CUsers).filter(CUsers.email == search).all()
                     self.set_status(201, 'Found')
-                    self.response['found_username'] = search_contact.username
-                    self.write_json()
-
-                elif search_username:
-                    search_contact = search_username
-                    self.set_status(201, 'Found')
-                    self.response['found_username'] = search_contact.username
+                    self.response['result'] = search_email
                     self.write_json()
 
                 else:
-                    self.set_status(404, 'User not found')
+                    search_username = self.db.query(CUsers).filter(CUsers.username == search).all()
+                    if search_username:
+                        self.set_status(201, 'Found')
+                        self.response['result'] = search_username
+                        self.write_json()
+
+                    else:
+                        self.set_status(404, 'User not found')
             except:
                 self.send_error(400, reason='No or bad request body')
